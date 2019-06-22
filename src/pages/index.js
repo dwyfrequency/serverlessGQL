2e0cb09f0b876d65354d9566eb0797c433df16b1
dwyfrequency <jdwy215@gmail.com>
2019-06-21 21:13:29 -0400

    adding useeffect hook with serverless func

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 import axios from "axios"
 
@@ -7,21 +7,18 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
+  const [apiData, setAPIData] = useState({})
   useEffect(() => {
-    return () => {
-      const fetchData = async () => {
-        const response = await axios(
-          "/.netlify/functions/hello-world?name=jack"
-        )
-        console.log(response)
-      }
-      fetchData()
+    const fetchData = async () => {
+      const { data } = await axios("/.netlify/functions/hello-world?name=jack")
+      setAPIData(data)
     }
+    fetchData()
   }, [])
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>Hi people</h1>
+      <h1>{apiData.message}</h1>
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
