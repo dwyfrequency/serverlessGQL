f7d9664510a76bcb91c6a40bed940828fea921d2
dwyfrequency <jdwy215@gmail.com>
2019-06-21 21:08:45 -0400

    adding fetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,35 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
+import axios from "axios"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  useEffect(() => {
+    return () => {
+      const fetchData = async () => {
+        const response = await axios(
+          "/.netlify/functions/hello-world?name=jack"
+        )
+        console.log(response)
+      }
+      fetchData()
+    }
+  }, [])
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
